d3168b4e90bf0c6e4d8560ff293efb84893b6526
Abhishek Shrestha <np01cp4a210122@islingtoncollege.edu.np>
2023-06-12 16:18:28 +0545

    Worked on welcome area of website: Incomplete

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { WelcomeArea } from './components/welcome-area-section/welcome-area.component';
 
 const App = () => {
   return (
-    < >
-      <p className='text-3xl font-bold  text-center'>Hello Evakon.Tech</p>
+    <>
+      {/* <p className='text-3xl font-bold  text-center'>Hello Evakon.Tech</p>
+       */}
+      <nav className='p-8 border border-solid border-black ' >
+      </nav>
+
+      <WelcomeArea />
     </>
   );
 }
