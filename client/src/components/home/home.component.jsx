be1b58f83a9e8ff9522a483326984da2b9dc598a
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-06-14 11:42:32 +0545

    removed-work-smart-section

--- client/src/components/home/home.component.jsx
+++ client/src/components/home/home.component.jsx
@@ -2,7 +2,6 @@
 import { WelcomeArea } from '../welcome-area-section/welcome-area.component';
 import { CollaboratingCompaniesList } from '../collaborating-company-list/collaborating-company-list.component';
 import { BusinessGrowthSection } from '../business-growth/business-growth.component';
-import { SmartWorkSection } from '../smart-work/smart-work-section.component';
 import { Footer } from '../footer/footer.component';
 import { Pricing } from '../pricing-section/pricing-section.component';
 import { Feature } from '../feature/Feature.jsx';
@@ -16,7 +15,6 @@ export const Home = () => {
             <CollaboratingCompaniesList />
             <Feature />
             <BusinessGrowthSection />
-            <SmartWorkSection />
             <Pricing />
             <FAQs />
             <CTA />
