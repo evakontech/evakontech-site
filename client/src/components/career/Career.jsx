002eb2be4a5d0af7fcf64f0fdae183cf42d05e8c
Sijal Manandhar <sijal.m06@gmail.com>
2023-06-15 16:12:15 +0545

    Career page changes: - Hero section updated - Company features section - Why join/About company section - removed unused styling

--- client/src/components/career/Career.jsx
+++ client/src/components/career/Career.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { NavigationBar } from '../navigation-bar/navigation-bar.component';
-import './career.css';
+import { Footer } from '../footer/footer.component';
 import { Link } from 'react-router-dom';
-import sideImage from '../../assets/career-side-img.jpg';
+import heroSectionImage from '../../assets/career-side-img.jpg';
+import aboutSectionImage from '../../assets/career-about-img.jpg';
+import { Code } from 'lucide-react';
+import { FaRegLightbulb } from 'react-icons/fa';
+import { MdOutlineDesignServices } from 'react-icons/md';
+import { HiOutlineRocketLaunch } from 'react-icons/hi2';
 
 const Career = () => {
     return (
         <>
-            <section className="career-page-wrapper">
-                <div className="w-full h-full navigation-container">
-                    <NavigationBar />
-                </div>
+            <div className="w-full h-full navigation-container">
+                <NavigationBar />
+            </div>
 
-                <div className="career-hero my-14 px-3 md:px-5">
-                    <div className="career-content">
-                        <p className="page-name text-xs md:text-base text-gray-500 mb-3 md:mb-4 font-medium ">
+            <section className="my-16">
+                <div className="container-width flex items-center ">
+                    <div className="w-full lg:w-4/5 m-auto text-center lg:text-left max-w-3xl lg:max-w-2xl">
+                        <p className="text-sm font-medium text-gray-400">
                             CAREERS
                         </p>
-                        <h1 className="text-4xl md:text-5xl font-bold mb-6 leading-tight max-w-2xl">
-                            <span className="text-slate-900">
-                                Be a part of a{' '}
-                            </span>
-                            <span className="colored-text">
-                                world class team.
-                            </span>
+                        <h1 className="text-4xl sm:text-6xl leading-tight font-bold max-w-2xl mb-5 mt-3 mx-auto lg:ms-0">
+                            <span className="text-slate-900">Join us at</span>
+                            <span className="text-[#1563CB]"> evakon.</span>
                         </h1>
+
                         <p className="text-slate-900 text-base mb-4 md:text-lg font-medium ">
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Nesciunt iusto repudiandae eligendi culpa
@@ -50,7 +52,6 @@ const Career = () => {
                                 See open positions
                             </button>
                         </Link>
-
                         <Link to="/career">
                             <button
                                 type="button"
@@ -60,11 +61,165 @@ const Career = () => {
                             </button>
                         </Link>
                     </div>
-                    <div className="career-img">
-                        <img src={sideImage} alt="" />
+                    {/* /hero-content */}
+
+                    <div className="w-full lg:w-1/2 px-5 text-center hidden lg:block">
+                        <img
+                            src={heroSectionImage}
+                            alt="Company"
+                            className="w-full"
+                        />
                     </div>
+                    {/* /hero-image */}
                 </div>
+                {/* /section-container */}
             </section>
+            {/* /hero-section */}
+
+            <section className="bg-gray-200 py-10 px-3">
+                <div className="section-container max-w-6xl mx-auto">
+                    <div className="mx-auto max-w-4xl text-center">
+                        <p className="text-sm font-medium text-gray-400">
+                            COMPANY'S VALUE
+                        </p>
+                        <h2 className="mt-3 text-3xl font-semibold leading-tight text-black md:text-4xl">
+                            Our culture drives our business and our employees
+                            are the most important{' '}
+                            <span className="text-[#1563CB]">
+                                piece of the puzzle.
+                            </span>
+                        </h2>
+                        <p className="mt-4 text-base leading-relaxed text-gray-600">
+                            From Concept to Code: Shaping Software Excellence.
+                        </p>
+                        <div className="mx-auto inline-flex rounded-full bg-[#1563CB] px-5 p-[0.15rem]"></div>
+                    </div>
+                    {/* /content */}
+
+                    <div className="mt-12 grid grid-cols-1 gap-y-6 text-center lg:grid-cols-2 sm:gap-12 place-items-center">
+                        <div className="bg-white w-full py-12 rounded-xl hover:shadow-lg transition px-3 max-w-lg lg:max-w-full">
+                            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
+                                <FaRegLightbulb className="h-9 w-9 text-white" />
+                            </div>
+                            <h3 className="mt-8 text-2xl font-semibold text-black">
+                                Idea & Analysis
+                            </h3>
+                            <p className="mt-4 text-md text-gray-600 max-w-sm mx-auto">
+                                Brainstorming ideas and evaluating project
+                                requirements to ensure a comprehensive
+                                understanding before proceeding.
+                            </p>
+                        </div>
+                        {/* /card-item */}
+                        <div className="bg-white w-full py-12 rounded-xl hover:shadow-lg transition px-3 max-w-lg lg:max-w-full">
+                            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
+                                <MdOutlineDesignServices className="h-9 w-9 text-white" />
+                            </div>
+                            <h3 className="mt-8 text-2xl font-semibold text-black">
+                                Designing
+                            </h3>
+                            <p className="mt-4 text-md text-gray-600 max-w-sm mx-auto">
+                                Creating visually appealing and user-friendly
+                                designs to enhance the overall aesthetic and
+                                usability of the project.
+                            </p>
+                        </div>
+                        {/* /card-item */}
+                        <div className="bg-white w-full py-12 rounded-xl hover:shadow-lg transition px-3 max-w-lg lg:max-w-full">
+                            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
+                                <Code className="h-9 w-9 text-white" />
+                            </div>
+                            <h3 className="mt-8 text-2xl font-semibold text-black">
+                                Development
+                            </h3>
+                            <p className="mt-4 text-md text-gray-600 max-w-sm mx-auto">
+                                Implementing and coding the project,
+                                transforming ideas into a functional and
+                                efficient end product.
+                            </p>
+                        </div>
+                        {/* /card-item */}
+                        <div className="bg-white w-full py-12 rounded-xl hover:shadow-lg transition px-3 max-w-lg lg:max-w-full">
+                            <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
+                                <HiOutlineRocketLaunch className="h-9 w-9 text-white" />
+                            </div>
+                            <h3 className="mt-8 text-2xl font-semibold text-black">
+                                Testing & Launching
+                            </h3>
+                            <p className="mt-4 text-md text-gray-600 max-w-sm mx-auto">
+                                Conducting rigorous testing procedures to ensure
+                                the project's functionality and a successful
+                                deployment to the intended audience.
+                            </p>
+                        </div>
+                        {/* /card-item */}
+                    </div>
+                    {/* /card-container */}
+                </div>
+                {/* /section-container */}
+            </section>
+            {/* /company-features-section */}
+
+            <section className="py-10 px-3">
+                <div className="container-width flex items-center max-w-3xl lg:max-w-full gap-x-10">
+                    <div className="w-full lg:w-1/2 px-5 text-center hidden lg:block">
+                        <img
+                            src={aboutSectionImage}
+                            alt="Company"
+                            className="w-full rounded-2xl shadow-xl"
+                        />
+                    </div>
+                    {/* /section-image */}
+
+                    <div className="w-full lg:w-4/5 m-auto text-center lg:text-left">
+                        <h2 className="text-4xl leading-tight font-semibold mb-3">
+                            <span className="text-slate-900">Be a part of</span>
+                            <span className="text-[#1563CB]"> our team.</span>
+                        </h2>
+
+                        <p className="text-slate-600 text-sm md:text-base">
+                            <p className="mb-2">
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Perspiciatis aliquam
+                                doloremque animis id tempore possimus ducimus
+                                animi minus!
+                            </p>
+                            <p>
+                                Reiciendis commodi vel tenetur repellat quae,
+                                delectus nostrum quam totam itaque tempore non
+                                exercitationem consequatur sed at! Placeat
+                                molestiae exercitationem suscipit pariatur
+                                corrupti, odio incidunt totam a voluptates odit
+                                alias facilis. Blanditiis repellendus sunt ullam
+                                rem eveniet atque aut consequatur quam deleniti
+                                fuga ab.
+                            </p>
+                        </p>
+                    </div>
+                    {/* /section-content */}
+                </div>
+                {/* /section-container */}
+            </section>
+            {/* about-company-section */}
+
+            <section className="bg-gray-200 py-10 px-3">
+                <div className="section-container max-w-6xl mx-auto">
+                    <div className="mx-auto max-w-4xl text-center">
+                        <p className="text-sm font-medium text-gray-400">
+                            INTERESTED ? WE ARE HIRING
+                        </p>
+                        <h2 className="mt-3 text-3xl font-semibold leading-tight text-black md:text-4xl">
+                            Current Openings
+                        </h2>
+                        <div className="mx-auto inline-flex rounded-full bg-[#1563CB] px-5 p-[0.15rem]"></div>
+                    </div>
+                    {/* /content */}
+                </div>
+                {/* /section-container */}
+            </section>
+            {/* /openings-section */}
+
+            <Footer />
         </>
     );
 };
--- client/src/components/feature/Feature.jsx
+++ client/src/components/feature/Feature.jsx
@@ -1,15 +1,14 @@
-import React from 'react'
-import { Code } from 'lucide-react'
-import {FaRegLightbulb} from 'react-icons/fa'
-import {MdOutlineDesignServices} from 'react-icons/md'
-import {HiOutlineRocketLaunch} from 'react-icons/hi2'
+import React from 'react';
+import { Code } from 'lucide-react';
+import { FaRegLightbulb } from 'react-icons/fa';
+import { MdOutlineDesignServices } from 'react-icons/md';
+import { HiOutlineRocketLaunch } from 'react-icons/hi2';
 
 export const Feature = () => {
     return (
         <div className="mx-auto max-w-7xl px-4 mb-[200px] sm:px-6 lg:px-8">
             <div className="mx-auto max-w-xl text-center">
-                <div className="mx-auto inline-flex rounded-full bg-gray-100 px-4 py-1.5">
-                </div>
+                <div className="mx-auto inline-flex rounded-full bg-gray-100 px-4 py-1.5"></div>
                 <h2 className="mt-6 text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl">
                     Building Bridges between Ideas and Execution
                 </h2>
@@ -22,39 +21,54 @@ export const Feature = () => {
                     <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
                         <FaRegLightbulb className="h-9 w-9 text-white" />
                     </div>
-                    <h3 className="mt-8 text-2xl font-semibold text-black">Idea & Analysis</h3>
+                    <h3 className="mt-8 text-2xl font-semibold text-black">
+                        Idea & Analysis
+                    </h3>
                     <p className="mt-4 text-md text-gray-600">
-                        Brainstorming ideas and evaluating project requirements to ensure a comprehensive understanding before proceeding.
+                        Brainstorming ideas and evaluating project requirements
+                        to ensure a comprehensive understanding before
+                        proceeding.
                     </p>
                 </div>
                 <div>
                     <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
                         <MdOutlineDesignServices className="h-9 w-9 text-white" />
                     </div>
-                    <h3 className="mt-8 text-2xl font-semibold text-black">Designing</h3>
+                    <h3 className="mt-8 text-2xl font-semibold text-black">
+                        Designing
+                    </h3>
                     <p className="mt-4 text-md text-gray-600">
-                        Creating visually appealing and user-friendly designs to enhance the overall aesthetic and usability of the project.
+                        Creating visually appealing and user-friendly designs to
+                        enhance the overall aesthetic and usability of the
+                        project.
                     </p>
                 </div>
                 <div>
                     <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
                         <Code className="h-9 w-9 text-white" />
                     </div>
-                    <h3 className="mt-8 text-2xl font-semibold text-black">Development</h3>
+                    <h3 className="mt-8 text-2xl font-semibold text-black">
+                        Development
+                    </h3>
                     <p className="mt-4 text-md text-gray-600">
-                        Implementing and coding the project, transforming ideas into a functional and efficient end product.
+                        Implementing and coding the project, transforming ideas
+                        into a functional and efficient end product.
                     </p>
                 </div>
                 <div>
                     <div className="mx-auto flex h-20 w-20 items-center justify-center rounded-full bg-[#1563cb]">
                         <HiOutlineRocketLaunch className="h-9 w-9 text-white" />
                     </div>
-                    <h3 className="mt-8 text-2xl font-semibold text-black">Testing & Launching</h3>
+                    <h3 className="mt-8 text-2xl font-semibold text-black">
+                        Testing & Launching
+                    </h3>
                     <p className="mt-4 text-md text-gray-600">
-                        Conducting rigorous testing procedures to ensure the project's functionality and a successful deployment to the intended audience.
+                        Conducting rigorous testing procedures to ensure the
+                        project's functionality and a successful deployment to
+                        the intended audience.
                     </p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
