f6ce6ded86f6e6d23b73d1a76efebcc538d5196e
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-06-19 18:47:02 +0545

    added multi step form component

--- client/src/components/multistep-form/MultistepForm.jsx
+++ client/src/components/multistep-form/MultistepForm.jsx
@@ -23,7 +23,7 @@ export const MultistepForm = () => {
 
 
     return (
-        <section className="bg-white flex flex-col min-h-screen max-w-5xl mx-auto md:my-4 md:min-h-[min(calc(100vh-2em),37.5em)] md:grid md:grid-cols-[17em,1fr] md:grid-rows-[1fr,auto] md:shadow-xl">
+        <section className="bg-white flex flex-col min-h-screen max-w-5xl mx-auto md:my-4 md:min-h-[min(calc(100vh-2em),37.5em)] md:grid md:grid-cols-[17em,1fr] md:grid-rows-[1fr,auto] md:shadow-sm">
             <Progress currentStepNo={stepNo} />
             <main className="flex-grow md:col-start-2 bg-magnolia md:w-[28.5em] md:justify-self-center md:bg-white">
                 {isStepComplete ? (
