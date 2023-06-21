ad2efe76725ae9c246da7e19f734ede516f21dce
Abhishek Shrestha <np01cp4a210122@islingtoncollege.edu.np>
2023-06-21 11:45:37 +0545

    added multi-step form as model

--- client/src/components/hire-developer/HireDeveloperCTA.jsx
+++ client/src/components/hire-developer/HireDeveloperCTA.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-export const CTA = () => {
+
+
+export const HireDeveloperCTA = ({ setDisplayMultiStepForm }) => {
     return (
         <section className="py-20 bg-gray-100">
             <div className="mx-auto max-w-7xl">
                 <div className="mx-auto max-w-2xl  text-center">
+
                     <div className="isolate flex justify-center -space-x-2">
                         <img
                             className="relative z-30 inline-block h-14 w-14 rounded-full ring-4 ring-white"
@@ -29,19 +32,20 @@ export const CTA = () => {
                     </div>
 
                     <h2 className="mt-8 text-3xl font-bold leading-tight text-black sm:text-4xl lg:mt-12 lg:text-5xl">
-                        Join <span className="border-b-8 text-[#1563cb] border-yellow-300">8+</span> other developers
+                        Hire <span className="border-b-8 text-[#1563cb] border-yellow-300">Top-notch</span>  Developers
                     </h2>
                     <p className="mx-auto mt-6 max-w-xl text-base text-gray-600 md:mt-10 lg:text-xl">
                         We are committed to promoting the remote work in Nepal's experience and shaping the future of the software industry.
                     </p>
-                    <Link to="/career">
-                        <button
-                            type="button"
-                            className="mt-8 rounded-md bg-[#1563cb] px-4 py-3 text-md font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-                        >
-                            Join our team
-                        </button>
-                    </Link>
+
+                    <button
+                        type="button"
+                        className="mt-8 rounded-md bg-[#1563cb] px-4 py-3 text-md font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+                        onClick={() => setDisplayMultiStepForm(true)}
+                    >
+                        Join our team
+                    </button>
+
 
                 </div>
             </div>
