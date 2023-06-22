96768cfe75e9529076f424f2e20bf9324b40aca7
Sijal Manandhar <sijal.m06@gmail.com>
2023-06-22 13:57:28 +0545

    stats & footerLinks in "constants"

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -20,3 +20,83 @@ export const navLink = [
         destination: '//hire-developer',
     },
 ];
+
+export const stats = [
+    {
+        id: 'stats-1',
+        title: 'Happy Clients',
+        value: '10+',
+    },
+    {
+        id: 'stats-2',
+        title: 'Project Delivered',
+        value: '5+',
+    },
+    {
+        id: 'stats-3',
+        title: 'Years Experience',
+        value: '1+',
+    },
+];
+
+export const footerLink = [
+    {
+        title: 'Company',
+        links: [
+            {
+                name: 'About us',
+                destination: '/about',
+            },
+            {
+                name: 'Our Team',
+                destination: '/about',
+            },
+            {
+                name: 'Our Vision',
+                destination: '/vision',
+            },
+            {
+                name: 'Our Products',
+                destination: '/products',
+            },
+        ],
+    },
+    {
+        title: 'Join Us',
+        links: [
+            {
+                name: 'Hire Remote Developer',
+                destination: '/hire-remote-developer',
+            },
+            {
+                name: 'Career at Evakon',
+                destination: '/career',
+            },
+            {
+                name: 'Open Source',
+                destination: '/open-source',
+            },
+        ],
+    },
+    {
+        title: 'Connection',
+        links: [
+            {
+                name: 'Blog',
+                destination: '/blog',
+            },
+            {
+                name: 'LinkedIn',
+                destination: 'https://www.linkedin.com/company/evakontech/',
+            },
+            {
+                name: 'Facebook',
+                destination: 'https://www.facebook.com/',
+            },
+            {
+                name: 'Contact Us',
+                destination: 'mailto:info@evakon.tech',
+            },
+        ],
+    },
+];
