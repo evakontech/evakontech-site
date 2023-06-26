2c42702fea5a1223a0fbb903386da7127af7ebec
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-06-26 23:27:41 +0545

    check-bot

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,21 @@
+import { useEffect } from 'react';
 import { Welcome, Collaboration, Footer, Feature, CTA, FAQs, Services, Testimonials } from '../components';
 import { TrustBox } from '../components/TrustPilot.jsx';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 const Home = () => {
+    useEffect(() => {
+        const chatbox = document.getElementById('fb-root');
+        if (chatbox) {
+            chatbox.style.position = 'fixed';
+            chatbox.style.bottom = '20px'; // Adjust the value to your desired position from the bottom
+            chatbox.style.right = '20px'; // Adjust the value to your desired position from the right
+        }
+    }, []);
+
     return (
         <>
             <Welcome />
-            <MessengerCustomerChat
-                pageId="111903781926107"
-                appId="223269043853889"
-            />
             <Collaboration />
             <Feature />
             <Services />
@@ -16,7 +23,12 @@ const Home = () => {
             <Testimonials />
             <TrustBox />
             <CTA />
-
+            <div id="fb-root">
+                <MessengerCustomerChat
+                    pageId="111903781926107"
+                    appId="223269043853889"
+                />
+            </div>
             <Footer />
         </>
     );
