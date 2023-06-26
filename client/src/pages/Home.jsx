de95dd0a9cab2cb1910260e7d38643aa0c3b2daf
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-06-26 23:15:36 +0545

    chat-bot

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Welcome, Collaboration, Footer, Feature, CTA, FAQs, Services, Testimonials } from '../components';
 import { TrustBox } from '../components/TrustPilot.jsx';
-
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 const Home = () => {
     return (
         <>
@@ -12,6 +12,10 @@ const Home = () => {
             <Testimonials />
             <TrustBox />
             <CTA />
+            <MessengerCustomerChat
+                pageId="111903781926107"
+                appId="223269043853889"
+            />
             <Footer />
         </>
     );
