605b1f8e97c6d472241e0bea61e5bbb48ff48491
Sudip Kumar Mahato <1234sudipmahato@gmail.com>
2023-06-26 23:31:13 +0545

    chat-bot-fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,8 @@
-import { useEffect } from 'react';
 import { Welcome, Collaboration, Footer, Feature, CTA, FAQs, Services, Testimonials } from '../components';
 import { TrustBox } from '../components/TrustPilot.jsx';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Home = () => {
-    useEffect(() => {
-        const chatbox = document.getElementById('fb-root');
-        if (chatbox) {
-            chatbox.style.position = 'fixed';
-            chatbox.style.bottom = '20px'; // Adjust the value to your desired position from the bottom
-            chatbox.style.right = '20px'; // Adjust the value to your desired position from the right
-        }
-    }, []);
-
     return (
         <>
             <Welcome />
